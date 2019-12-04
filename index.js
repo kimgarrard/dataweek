4eee4a4b4507bdb56f8a2c85effb84b226db6c24
Kim <kimgarrard030@gmail.com>
2019-12-04 13:49:27 +0100

    Update kaart basis

---
+++ index.js
@@ -0,0 +1,15 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+
+let optie1 = document.querySelector(".optie1");
+let optie2 = document.querySelector(".optie2");
+let optie3 = document.querySelector(".optie3");
+let optie4 = document.querySelector(".optie4");
+let optie5 = document.querySelector(".optie5");
+
+let optie1img = document.querySelector(".optie1img");
+let optie2img = document.querySelector(".optie2img");
+let optie3img = document.querySelector(".optie3img");
+let optie4img = document.querySelector(".optie4img");
+let optie5img = document.querySelector(".optie5img");
